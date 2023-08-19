f9f0036c5bd845701533c1e29448fdd6d2a7fd53
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-19 20:12:28 +0300

    Добавлены атрибуты при инициалиции класса

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,20 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.getenv("YT_API_KEY")
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.subscribers = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=self.api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
