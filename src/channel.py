da664ef897cf7e7f9f406f94e352da0704a3de71
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-08-19 20:33:09 +0300

    метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра `Channel`

--- src/channel.py
+++ src/channel.py
@@ -10,7 +10,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
@@ -25,4 +25,22 @@ class Channel:
 
     @classmethod
     def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
         return cls.youtube
+
+    def to_json(self, file_name: str) -> None:
+        """
+        Сохраняет в файл значения атрибутов экземпляра `Channel`
+        """
+        data = {"channel_id": str(self.channel_id),
+                "title": self.title,
+                "description": str(self.description),
+                "video_count": self.video_count,
+                "subscribers": self.subscribers,
+                "url": self.url
+
+                }
+        with open(file_name, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False)
